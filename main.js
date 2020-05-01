e4b862ff12a123b69f2b679b45d1f06495a212e4
liyang <1909457483@qq.com>
2020-05-01 22:28:56 +0800

    release: 新增 autoUpdater

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const { app } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
-
+const { autoUpdater } = require("electron-updater");
 const AppWindow = require("./src/main/AppWindow");
 
 let mainWindow = null;
 app.on("ready", () => {
+  if (isDev) {
+    autoUpdater.updateConfigPath = path.join(__dirname, "app-update.yml");
+  }
+  autoUpdater.checkForUpdates();
+  autoUpdater.on("error", function (error) {
+    console.log("[UPDATE ERROR!!!]:", error);
+  });
+  autoUpdater.on("update-available", function (updateInfo) {
+    console.log("[update-available]:", updateInfo);
+  });
+  autoUpdater.on("update-not-available", function (info) {
+    console.log("[update-not-available]:", info);
+  });
+
   const mainWindowConfig = {
     width: 1440,
     height: 768,
